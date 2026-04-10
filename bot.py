1f39519a73a79f883fc57fdccf9f39974296a7ed
avonmims <avonmims@gmail.com>
2026-04-10 17:47:26 -0400

    mp3_clip directory variable establishment WIP

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 import os
-from dotenv import load_dotenv # load variables from .env file
+import random
+import pathlib
+from dotenv import load_dotenv
 from discord.ext import commands
 
+# -- load bot token from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# -- establish mp3_clips directory path
+clips_dir = pathlib.Path(__file__).parent / 'mp3_clips'
 # -- hell yea new beningings lol, above line is for loading super secret bot token
